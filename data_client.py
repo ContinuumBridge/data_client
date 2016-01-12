5b3b9d9e4dea1142e5e52a089e1b1e7e3f5bfc07
PeterClaydon <peter.claydon@continuumbridge.com>
2016-01-12 10:51:22 +0000

    Logging changes

--- data_client.py
+++ data_client.py
@@ -41,7 +41,7 @@ CONFIG_READ_INTERVAL  = 10.0
 CB_LOGGING_LEVEL      = "DEBUG"
 logger = logging.getLogger('Logger')
 logger.setLevel(CB_LOGGING_LEVEL)
-handler = logging.handlers.RotatingFileHandler(CB_LOGFILE, maxBytes=10000000, backupCount=5)
+handler = logging.handlers.RotatingFileHandler(CB_LOGFILE, maxBytes=10000000, backupCount=3)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
